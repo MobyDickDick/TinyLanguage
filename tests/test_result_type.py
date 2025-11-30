93792fe2e635305447e52707a062d96c046804bd
MobyDickDick <72887495+MobyDickDick@users.noreply.github.com>
2025-11-30 18:45:03 +0100

    Move source files into src directory

--- tests/test_result_type.py
+++ tests/test_result_type.py
@@ -3,7 +3,7 @@ import sys
 
 import pytest
 
-sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1] / "src"))
 
 from tiny_language import compile_and_run
 

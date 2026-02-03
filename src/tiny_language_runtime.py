a25a165309214002c2393c7fe233f2e8ad8dcb7a
MobyDickDick <72887495+MobyDickDick@users.noreply.github.com>
2026-02-03 20:15:37 +0100

    Improve runtime error span formatting

--- src/tiny_language_runtime.py
+++ src/tiny_language_runtime.py
@@ -707,13 +707,18 @@ class Runtime:
         return resolved_pos, resolved_span
 
     @staticmethod
-    def _format_location(pos: Optional[SourcePos], span: Optional[SourceSpan]) -> Optional[Union[SourcePos, SourceSpan]]:
+    def _format_location(
+        pos: Optional[SourcePos], span: Optional[SourceSpan]
+    ) -> Optional[Union[SourcePos, SourceSpan]]:
+        if span is not None:
+            if span.start != span.stop:
+                return span
+            if pos is None:
+                return span.start
         if pos is not None:
             return pos
         if span is not None:
-            if span.start.line == span.stop.line:
-                return span.start
-            return span
+            return span.start
         return pos
 
     def _record_error(
